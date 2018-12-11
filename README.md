c6a459edbede482ddb28d2f9ace9f827e6cab33c
Randy Gingeleski <rjg26247@gmail.com>
2018-12-10 21:49:34 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,24 @@
+
+# libreveal.js
+
+Reveals third-party JavaScript libraries in use.
+
+Developed as a hacking tool. You should know what libraries are in use for your own apps, probably.
+
+## Usage
+
+Execute the contents of [**libreveal.min.js**](https://github.com/gingeleski/libreveal.js/blob/master/libreveal.min.js) on your target page via the developer tools console.
+
+See the unminified [**libreveal.js**](https://github.com/gingeleski/libreveal.js/blob/master/libreveal.js) script if you're curious how this works and/or what checks are run.
+
+## Support libraries
+
+Currently (v1.0) this will detect...
+
+- jQuery
+- Angular
+- Bootstrap
+
+## Credit
+
+Derived in part from [**retire.js**](https://github.com/retirejs/retire.js/) identifiers - this stands on the shoulders of that project.
---
+++ libreveal.js
@@ -0,0 +1,31 @@
+// libreveal.js v1.0
+
+//jquery
+if (typeof jQuery !== 'undefined' && typeof jQuery.fn !== 'undefined')
+{
+    console.log('libreveal.js: jquery @ ' + jQuery.fn.jquery);
+}
+else if (typeof $ !== 'undefined' && typeof $.fn !== 'undefined')
+{
+    console.log('libreveal.js: jquery @ ' + $.fn.jquery);
+}
+else if (typeof $j !== 'undefined' && typeof $j.fn !== 'undefined')
+{
+    console.log('libreveal.js: jquery @ ' + $j.fn.jquery);
+}
+else if (typeof $jq !== 'undefined' && typeof $jq.fn !== 'undefined')
+{
+    console.log('libreveal.js: jquery @ ' + $jq.fn.jquery);
+}
+
+// angularjs
+if (typeof angular !== 'undefined' && typeof angular.version !== 'undefined')
+{
+    console.log('libreveal.js: angularjs @ ' + angular.version.full);
+}
+
+// bootstrap
+if (typeof $ !== 'undefined' && typeof $.fn !== 'undefined' && typeof $.fn.tooltip !== 'undefined' && typeof $.fn.tooltip.Constructor !== 'undefined')
+{
+    console.log('libreveal.js: bootstrap @ ' + $.fn.tooltip.Constructor.VERSION);
+}
